d2d5aaf140f57f277c77d13ec505a1ddab19aba5
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-04-11 01:36:17 -0700

    ae

--- app.js
+++ app.js
@@ -15,3 +15,5 @@ console.log(myName, myAge); //inspect console
 console.log(myChildren); // Array [] 5. ja et kui teed [0] on see esimene nimi
 console.log(CanYouRunFasterThanOtto); // Boolean 6.
 console.log(person); // saad panna naideks myName sinna (person.myName) et see oleks erinev key value.
+
+//VARIABLE SCOPE

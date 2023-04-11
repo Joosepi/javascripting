f54064800095b0326d0695a5206dbb1bff084782
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-04-11 00:57:41 -0700

    aee

---
+++ app.js
@@ -0,0 +1,17 @@
+// Variables
+
+const myName = "Martin Joosep Reiljan"; //String
+let myAge = 17; //Number
+const myChildren = ["Toomas","Hendrick"]; // array
+const CanYouRunFasterThanOtto = 'False'; // Boolean 
+const person = {
+    myName: 'Martin Joosep Reiljan',
+    myAge: 17,
+}; //Object
+
+
+
+console.log(myName, myAge); //inspect console
+console.log(myChildren); // Array [] 5.
+console.log(CanYouRunFasterThanOtto); // Boolean 6.
+console.log(person) // saad panna naideks myName sinna (person.myName) et see oleks erinev key value.

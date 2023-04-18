7b72320a3c334c541c411727f00b9a6aceee3cb5
Joosepi <martinjoosep.reiljan@ametikool.ee>
2023-04-17 23:54:22 -0700

    aee

---
+++ beginnerjs.js
@@ -0,0 +1,25 @@
+// Objects
+
+const car = {
+    mark: 'Ferrari',
+    model: 'Ferrari S9',
+    transmission: 'Manual',
+    type: 'Driveable',
+    color: {
+        black: 500
+    },
+};
+
+const numbers = [1,2,3,4,5];
+const birthDay = '12';
+const birthdaymonth = 'June';
+
+console.log(typeof birthDay)  // näidab andmetüüpi
+
+console.log(new Date());
+console.log(Math.random());
+console.log(typeof Number(birthdaymonth));
+
+
+// new section 2 - Explicit vs Implicit
+
